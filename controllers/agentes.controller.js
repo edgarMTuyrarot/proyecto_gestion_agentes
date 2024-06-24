4f850eb166b30a34420240350d5d01acbdc27451
Edgar Tuyarot <edgar.m.tuyarot@gmail.com>
2024-06-24 00:24:28 -0300

    Agent controller, whit orm

--- controllers/agentes.controller.js
+++ controllers/agentes.controller.js
@@ -2,7 +2,7 @@ import { pool } from "../src/db.js";
 import { Agentes } from "../src/models/Agentes.js";
 
 
-//Controller for find all agents
+//Controller to get all agents
 export const getAgentes = async (req, res) => {
   try {
     const agentes = await Agentes.findAll({where:{deleted:0}})
@@ -15,23 +15,15 @@ export const getAgentes = async (req, res) => {
   }
 };
 
-//Controlador de un agente en particular requiere el id por parametros
+//Controller to find an Agenten by id
 export const getAgente = async (req, res) => {
   try {
     const id = req.params.id;
-    const [rows] = await pool.query(
-      "SELECT * FROM agentes WHERE id = (?) AND deleted != 1",
-      id
-    );
-    if (rows.length <= 0) {
-      res.status(404).json({
-        message: "Sin Agentes",
-      });
-    }
-    res.json(rows[0]);
+    const agente = await Agentes.findByPk(id)
+    res.json(agente);
   } catch (error) {
     res.status(500).json({
-      message: "algo salio mal",
+      message: error,
     });
   }
 };
@@ -60,7 +52,8 @@ export const postAgente = async (req, res) => {
       dni,
       telefono,
       mail,
-      sups_id
+      sups_id,
+      deleted:0
     })
 
     res.json(agente);
@@ -71,7 +64,7 @@ export const postAgente = async (req, res) => {
   }
 };
 
-//Controlador para actualizar algun campo de algun agente en particular, mediante meteodo patch en ruta
+//Controller to update info of an agente.
 export const updateAgente = async (req, res) => {
   try {
     const { id } = req.params;
@@ -110,23 +103,18 @@ export const updateAgente = async (req, res) => {
   }
 };
 
-//Controlador para borrar agente de la tabla, solo se cambia el valor del campo deleted del registro a 1
+//Controller to delete an agent, only change 'deteled' field from 0 to 1 (logic ereaser)
 export const deleteAgentes = async (req, res) => {
   try {
     const { id } = req.params;
-    const [result] = await pool.query(
-      "UPDATE agentes SET deleted = 1 WHERE id=?",
-      [id]
-    );
-    if (result.affectedRows === 0) {
-      res.status(404).json({
-        message: "Agente no encontrado",
-      });
-    }
-    res.json(result);
+    const agente = await Agentes.findByPk(id)
+    agente.update({deleted:1})
+    res.status(200).json({
+      message:"agente borrado"
+    });
   } catch (error) {
     res.status(500).json({
-      message: "algo salio mal",
+      message: error,
     });
   }
 };
