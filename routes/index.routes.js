7b55be9ddb3301f0ebe14d5e2efad46007fd2c02
Edgar Tuyarot <edgar.m.tuyarot@gmail.com>
2024-06-26 09:48:45 -0300

    add roles, users, modif jccs

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,15 +1,12 @@
 import { Router } from "express";
-import { ping } from "../controllers/index.controllers.js";
+
 
 
 const router = Router();
 
 router.get("/",(req,res)=>{
-    res.json({
-        author:"Edgar Tuyarot",
-        description:"CRM Empleados",
-        version:"1.0"
-    })
+    res.render('index',{name:"GI"})
+
 } );
 
 export default router;
--- src/app.js
+++ src/app.js
@@ -1,41 +1,34 @@
 import express from "express";
-import morgan from 'morgan'
+import morgan from "morgan";
 import agentesRoutes from "../routes/agentes.routes.js";
-import supsRoutes from '../routes/sups.routes.js'
+import supsRoutes from "../routes/sups.routes.js";
 import indexRouter from "../routes/index.routes.js";
-import jccsRoutes from "../routes/jccs.routes.js";
 import devolucionesRoutes from "../routes/devoluciones.routes.js";
 import fcrRoutes from "../routes/fcr.routes.js";
-import cuartilesRoutes from '../routes/cuartiles.routes.js'
-import perfilamientosRoutes from '../routes/perfilamientos.routes.js'
-import clustersRoutes from '../routes/clusters.routes.js'
-import {PORT} from './config.js'
-
-
+import cuartilesRoutes from "../routes/cuartiles.routes.js";
+import perfilamientosRoutes from "../routes/perfilamientos.routes.js";
+import clustersRoutes from "../routes/clusters.routes.js";
 
 const app = express();
 
-
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(express.json());
 
 //Usando rutas
 app.use(indexRouter);
-app.use(agentesRoutes);
-app.use(supsRoutes);
-app.use(jccsRoutes);
-app.use("/devoluciones",devolucionesRoutes);
 app.use(fcrRoutes);
-app.use(cuartilesRoutes);
-app.use(clustersRoutes);
-app.use(perfilamientosRoutes);
-
+app.use("/agentes", agentesRoutes);
+app.use("/sups", supsRoutes);
+app.use("/devoluciones", devolucionesRoutes);
+app.use("/cuartiles", cuartilesRoutes);
+app.use("/clusters", clustersRoutes);
+app.use("/perfilamientosRoutes", perfilamientosRoutes);
 
 
 app.use((req, res, netx) => {
-    res.status(404).json({
-        message: "endpoint no encontrado",
+  res.status(404).json({
+    message: "endpoint no encontrado",
   });
 });
 
-export default app
+export default app;
